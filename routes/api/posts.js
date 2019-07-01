3dc4ef6c88968c46678490ee1d11dcc53d6b8efe
adamGuinea <adamguinea22@gmail.com>
2019-07-01 23:56:55 +1200

    adding like/unlike functionality

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -99,12 +99,68 @@ router.delete("/:id", auth, async (req, res) => {
     }
 
     await post.remove();
+
     res.json({ msg: "Post removed" });
   } catch (error) {
     console.error(error.message);
     if (error.kind === "ObjectId") {
       return res.status(404).json({ msg: "Post not found" });
     }
+    res.status(500).send("Server Error");
+  }
+});
+
+// PUT to api/posts/like/:id
+// DESC like a post
+// ACCESS private
+
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).json({ msg: "Sorry, can't like twice" });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// PUT to api/posts/like/:id
+// DESC unlike a post
+// ACCESS private
+
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length ===
+      0
+    ) {
+      return res.status(400).json({ msg: "Post has not yet been liked" });
+    }
+
+    const removeIndex = post.likes
+      .map(like => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (error) {
+    console.error(error.message);
     res.status(500).send("Server Error");
   }
 });
