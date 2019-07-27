50eb0bce5a52178f734570fc8530c3f888cd4525
adamGuinea <adamguinea22@gmail.com>
2019-07-27 15:22:46 +1000

    rendering post timeline

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,6 +1,8 @@
 import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import PostItem from "./PostItem";
+import PostForm from "./PostForm";
 import Spinner from "../layout/Spinner";
 import { getPosts } from "../../actions/post";
 
@@ -8,7 +10,22 @@ const Post = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return <div>hi</div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Welcome to the community
+      </p>
+      <PostForm />
+      <div className='posts'>
+        {posts.map(post => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Post.propTypes = {
