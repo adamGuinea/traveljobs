7cd74ae2b88afc4dcaea1736e658f39e2dfe551d
adamGuinea <adamguinea22@gmail.com>
2019-12-23 20:54:24 +1000

    redesigning landing page

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -71,14 +71,14 @@ const AddEducation = ({ addEducation, history }) => {
         <div className='form-group'>
           <input
             type='text'
-            placeholder='Field of Study'
+            placeholder='* Field of Study'
             name='fieldofstudy'
             value={fieldofstudy}
             onChange={e => onChange(e)}
           />
         </div>
         <div className='form-group'>
-          <h4>From Date</h4>
+          <h4>* From Date</h4>
           <input
             type='date'
             name='from'
---
+++ controllers/profileController.js
@@ -0,0 +1,138 @@
+const express = require("express");
+const router = express.Router();
+const { check } = require("express-validator/check");
+
+const auth = require("../middleware/auth");
+const profileController = require("../controllers/profileController");
+
+/**
+ * @route   GET api/profile/me
+ * @desc    Get current user profile
+ * @access  Private
+ */
+router.get("/me", auth, profileController.getUserProfile);
+
+/**
+ * @route   POST api/profile
+ * @desc    Create or update user profile
+ * @access  Private
+ */
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required")
+        .not()
+        .isEmpty(),
+      check("skills", "Skills is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  profileController.createOrUpdateProfile
+);
+
+/**
+ * @route   GET api/profile
+ * @desc    Get all profiles
+ * @access  Public
+ */
+router.get("/", profileController.getProfiles);
+
+/**
+ * @route   GET api/profile/user/:userId
+ * @desc    Get user profile by id
+ * @access  Public
+ */
+router.get("/user/:userId", profileController.getUserProfileById);
+
+/**
+ * @route   DELETE api/profile
+ * @desc    Delete profile, user & posts for currently registered user
+ * @access  Private
+ */
+router.delete("/", auth, profileController.deleteProfileUserAndPosts);
+
+/**
+ * @route   PUT api/profile/experience
+ * @desc    Add profile experience of currently registered user
+ * @access  Private
+ */
+router.put(
+  "/experience",
+  [
+    auth,
+    [
+      check("title", "Title is required")
+        .not()
+        .isEmpty(),
+      check("company", "Company is required")
+        .not()
+        .isEmpty(),
+      check("from", "From date is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  profileController.addProfileExperience
+);
+
+/**
+ * @route   DELETE api/profile/experience/:experienceId
+ * @desc    Delete profile experience of currently registered user
+ * @access  Private
+ */
+router.delete(
+  "/experience/:experienceId",
+  auth,
+  profileController.deleteProfileExperience
+);
+
+/**
+ * @route   PUT api/profile/experience
+ * @desc    Add profile education of currently registered user
+ * @access  Private
+ */
+router.put(
+  "/education",
+  [
+    auth,
+    [
+      check("school", "School is required")
+        .not()
+        .isEmpty(),
+      check("degree", "Degree is required")
+        .not()
+        .isEmpty(),
+      check("fieldofstudy", "Field of study is required")
+        .not()
+        .isEmpty(),
+      check("from", "From date is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  profileController.addProfileEducation
+);
+
+/**
+ * @route   DELETE api/profile/education/:educationId
+ * @desc    Delete profile education of currently registered user
+ * @access  Private
+ */
+router.delete(
+  "/education/:educationId",
+  auth,
+  profileController.deleteProfileEducation
+);
+
+/**
+ * @route GET api/profile/github/:username
+ * @desc Get user repos from Github
+ * @access Public
+ */
+
+router.get("/github/:username", profileController.getUserGithubRepos);
+
+module.exports = router;
